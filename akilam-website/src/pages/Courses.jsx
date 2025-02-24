813e952403c667ea31a8acf20de24e870c6ec869
Deepan360 <deepandeep686@gmail.com>
2025-02-24 17:39:58 +0530

    box install

--- akilam-website/src/pages/Courses.jsx
+++ akilam-website/src/pages/Courses.jsx
@@ -371,7 +371,7 @@ const handleSubmit = async (e) => {
   try {
     const response = await fetch(
       "https://akilamwebserver.onrender.com/send-email",
-      {
+      {   
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(finalFormData),
